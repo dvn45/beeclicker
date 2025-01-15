9b8732831b1a3ee77e452f7188f738326d73cb75
dvn45 <90113786+dvn45@users.noreply.github.com>
2025-01-15 13:02:03 -0500

    added more buildings

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 const tickRate = 1000 / 30; // 30 FPS
 let score = 0;
 
-let bees = new Building('Bees', 0.1, 15, 'buybees');
-let advancedBees = new Building('Advanced Bees', 1, 100, 'buyAdvancedBees');
+let bees = new Building('Bees', 0.1, 15, 'buyBees');
+let advancedBees = new Building('Advanced Bees', 1, 50, 'buyAdvancedBees');
+let strongBees = new Building('Strong Bees', 2, 100, 'buyStrongBees');
+let queenBees = new Building('Queen Bees', 5, 250, 'buyQueenBees');
+let kingBees = new Building('King Bees', 15, 500, 'buyKingBees');
 
 function scorePlusPlus() {
     score++;
@@ -11,11 +14,17 @@ function scorePlusPlus() {
 function incScore() {
     score += bees.cps;
     score += advancedBees.cps;
+    score += strongBees.cps
+    score += queenBees.cps
+    score += kingBees.cps
 }
 
 function updateButtons() {
     bees.buttonState();
     advancedBees.buttonState();
+    strongBees.buttonState();
+    queenBees.buttonState();
+    kingBees.buttonState();
 }
 
 function updatePage() {
